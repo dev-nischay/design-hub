7d19901730d43c5394db04028e01cf9a9cdb339e
dev-nischay <nischay.mehn@gmail.com>
2025-10-14 18:41:00 +0530

    added further components

---
+++ landing-pages/bookmark-landing/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import heroImage from "../assets/hero-image.svg";
+import { Button } from "./Button";
+export const Hero = () => {
+  return (
+    <div>
+      <div className="mt-[4.2rem]">
+        <img src={heroImage} alt="image" />
+      </div>
+      <div className="flex flex-col gap-5 text-center ">
+        <div>
+          <h1 className="font-semibold text-3xl">A Simple Bookmark Manager</h1>
+        </div>
+        <div className="opacity-45 text-lg pt-5 text-slate-900  ">
+          A clean and simple interface to organize your favourite websites. Open
+          a new browser tab and see your sites load instantly. Try it for free.
+        </div>
+        <div className="flex justify-center gap-4 pt-5 ">
+          <Button text={"Get It On Chrome"} />
+          <Button
+            text={"Get It On Firefox"}
+            className="bg-[#D1D5DB] border-[#D1D5DB]  text-black  hover:border-neutral-300 hover:opacity-80 hover:text-black"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- landing-pages/bookmark-landing/src/components/Navbar.jsx
+++ landing-pages/bookmark-landing/src/components/Navbar.jsx
@@ -1,5 +1,17 @@
 import { TextAlignJustify } from "lucide-react";
+import logo from "../assets/nav-logo.svg";
 
 export const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-between mt-10 ">
+      <div>
+        <img src={logo} loading="lazy" />
+      </div>
+      <div>
+        <button className="hover:bg-slate-700 rounded-md transition-all ">
+          <TextAlignJustify size={24} absoluteStrokeWidth strokeWidth={2} />
+        </button>
+      </div>
+    </div>
+  );
 };
