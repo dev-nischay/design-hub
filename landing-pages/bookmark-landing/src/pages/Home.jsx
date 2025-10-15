1c6097dc9421b1a07f7e9ef2523ee65621722b30
dev-nischay <nischay.mehn@gmail.com>
2025-10-15 16:33:36 +0530

    updated home page

--- landing-pages/bookmark-landing/src/pages/Home.jsx
+++ landing-pages/bookmark-landing/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { Hero } from "../components/Hero";
+import { Extensions } from "../sections/Extenstions";
 import { Features } from "../sections/Features";
 
 export const Home = () => {
   return (
-    <div className="flex flex-col gap-[5.5rem]">
+    <div className="flex flex-col gap-[5.6rem]  ">
       <Hero />
       <Features />
+      <Extensions />
     </div>
   );
 };
