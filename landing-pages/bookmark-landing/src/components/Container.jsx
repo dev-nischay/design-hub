874ff5cdb38f59d1c6a714b6b2fd597cf5a85d66
dev-nischay <nischay.mehn@gmail.com>
2025-10-18 23:46:31 +0530

    added components

--- landing-pages/bookmark-landing/src/components/Container.jsx
+++ landing-pages/bookmark-landing/src/components/Container.jsx
@@ -1,7 +1,9 @@
 export const Container = ({ children }) => {
   return (
     <div className="bg-white min-h-screen  w-screen p-6">
-      <div className=" max-w-md mx-auto w-full font-[rubik]">{children}</div>
+      <div className="  mx-auto w-full font-[rubik] mb-28 sm:max-w-lg md:max-w-none">
+        {children}
+      </div>
     </div>
   );
 };
